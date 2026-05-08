de91986ed9ec67de7045998cf543dacb8066bdc5
atnatewoss <icodeyt21@gmail.com>
2026-05-08 15:34:50 +0300

    chore(server): export Router and default_router to public API

--- packages/pyrpc-server/src/pyrpc_server/__init__.py
+++ packages/pyrpc-server/src/pyrpc_server/__init__.py
@@ -1,17 +1,17 @@
 __version__ = "0.1.0"
 
 from .client.python_client import RPCClient, RPCError
-from .core.decorators import default_registry, rpc
+from .core.decorators import default_router, rpc
 from .core.introspection import get_procedure_schema, get_registry_schema
 from .core.interpreter import handle_request
 from .core.models import RpcRequest, RpcResponse
-from .core.registry import ProcedureRegistry
+from .core.registry import Router
 from .transport.asgi import PyRPCAsgiApp, app as asgi_app
 
 __all__ = [
-    "ProcedureRegistry",
+    "Router",
     "rpc",
-    "default_registry",
+    "default_router",
     "RpcRequest",
     "RpcResponse",
     "handle_request",
